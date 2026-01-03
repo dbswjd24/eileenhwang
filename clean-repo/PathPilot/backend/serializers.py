05f61ab97f5658ebac3615cf70cfac56282af9d7
Eileen Hwang <eileenhwang@berkeley.edu>
2026-01-02 18:54:08 -0600

    Remove clean-repo folder

--- clean-repo/PathPilot/backend/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Place
-
-class MySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Place
-        fields = '__all__'
-
-
--- clean-repo/PathPilot/backend/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import MyAPIView, MakeRecListView
-
-urlpatterns = [
-    path('', MyAPIView.as_view(), name='my-api'),
-    path('search/', MakeRecListView.as_view(), name='make-reclist'),
-]
